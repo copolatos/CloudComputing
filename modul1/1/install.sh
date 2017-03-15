6a1a3f441963458c2f17cba657a0f79ef31ad6dc
copolatos <dimsy_keren@me.com>
2017-03-15 16:47:18 +0700

    Update install.sh

--- modul1/1/install.sh
+++ modul1/1/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:ondrej/php
+sudo apt-get install python-software-properties -y
+sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
-sudo apt-get install apache2
+sudo apt-get install apache2 -y
 sudo apt-get install php7.0 php7.0-mcrypt php7.0-curl -y
